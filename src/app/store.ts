5f9059740c0c2846779a8daca2d7a86f9461425d
MariyaN23 <sellaite505@gmail.com>
2024-03-27 21:12:59 +0300

    redux toolkit

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,12 @@
-import {applyMiddleware, combineReducers, legacy_createStore} from 'redux';
+import {combineReducers} from 'redux';
 import {tasksReducer, TasksReducerType} from '../features/TodolistsList/TasksReducer';
 import {todolistReducer, TodolistReducerType} from '../features/TodolistsList/TodolistReducer';
-import {thunk, ThunkAction} from 'redux-thunk';
+import {ThunkAction} from 'redux-thunk';
 import {appReducer, AppReducerType} from './app-reducer';
 import {loginReducer, LoginReducerType} from '../features/Login/LoginReducer';
+import {configureStore} from '@reduxjs/toolkit';
 
-export type AppRootState = ReturnType<typeof rootReducer>
+export type AppRootState = ReturnType<typeof store.getState>
 export type AppActionsType = TasksReducerType | TodolistReducerType | AppReducerType | LoginReducerType
 export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, AppRootState, unknown, AppActionsType>
 
@@ -16,7 +17,11 @@ export const rootReducer = combineReducers({
     login: loginReducer,
 })
 
-export const store = legacy_createStore(rootReducer, applyMiddleware(thunk))
+//export const store = legacy_createStore(rootReducer, applyMiddleware(thunk))
+export const store = configureStore({
+    reducer: rootReducer
+})
+
 
 // @ts-ignore
 window.store = store
